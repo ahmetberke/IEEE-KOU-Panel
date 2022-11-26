3bc876df99ffd9e227d61777b2ca684b2a30f957
ahmetberke <tuncel.ahmet.berke@gmail.com>
2022-11-26 13:24:34 +0300

    fixed somethings

---
+++ src/components/Navbar/Logo.js
@@ -0,0 +1,11 @@
+import "./index.css"
+
+function Logo() {
+  return (
+    <div className="Logo">
+        IEEE KOU
+    </div>
+  );
+}
+
+export default Logo;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import "./index.css"
+import Logo from "./Logo";
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+        <Logo/>
+    </div>
+  );
+}
+
+export default Navbar;
